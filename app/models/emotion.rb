cc603ff452cf6ecb0b9c579381bdebdd1723e6a8
Andres Henriquez <rymbell@gmail.com>
2023-08-22 11:43:23 +0900

    seeded and modified emotion model

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -1,5 +1,5 @@
 class Emotion < ApplicationRecord
-  belongs_to :emotion
+  belongs_to :parent_emotion, optional: true, class_name: 'Emotion', foreign_key: :emotion_id, dependent: :destroy
   has_many :entries
   has_many :posts, through: :tags
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Emotion.destroy_all
+
+puts 'creating emotions and sub emotions'
+
+emotions = ["angry", "disgusted", "fearful", "bad", "surprised", "happy", "sad"]
+
+sub_emotions_list = [
+  ["let down", "humiliated", "bitter", "mad", "aggressive", "frustrated", "distant", "critical"],
+  ["disapproving", "disappointed", "awful", "repelled"],
+  ["scared", "anxious", "insecure", "weak", "rejected", "threatened"],
+  ["bored", "busy", "stressed", "tired"],
+  ["startled", "confused", "amazed", "excited"],
+  ["playful", "content", "interested", "proud", "accepted", "powerful", "peaceful", "trusting", "optimistic"],
+  ["lonely", "vulnerable", "despair", "guilty", "depressed", "hurt"]
+]
+
+emotions.each_with_index do |emotion, index|
+  instance = Emotion.create!(name: emotion)
+  sub_emotions_list[index].each do |sub_emotion|
+    sub_instance = Emotion.new(name: sub_emotion)
+    sub_instance.parent_emotion = instance
+    sub_instance.save
+  end
+end
+
+puts 'created emotions and sub emotions'
