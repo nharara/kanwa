ab5c93eca3594dcc73fecdcdef05d231c42ce46a
pj taron <jadepeck27@gmail.com>
2023-08-23 18:19:32 +0900

    heatmap2

--- app/javascript/controllers/apexchart_controller.js
+++ app/javascript/controllers/apexchart_controller.js
@@ -3,79 +3,20 @@ import ApexCharts from 'apexcharts';
 
 // Connects to data-controller="heatmap"
 export default class extends Controller {
-  static values={keys: Array, datas: Array}
+  static values={keys: Array, datas: Array, info: String}
   static targets=["heatmap", "pie", "donut"]
   connect() {
-    console.log(this.keysValue, this.datasValue)
-    function generateData(count, yrange) {
-      var i = 0;
-      var series = [];
-      while (i < count) {
-        var x = (i + 1).toString();
-        var y =
-        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-
-        series.push({
-          x: x,
-          y: y
-        });
-        i++;
-      }
-      return series;
-      }
-    // var heatmapOptions = {
-    //   chart: {
-    //     type: 'heatmap',
-    //     // toolbar: {show:false}
-    //   },
-    //   series: [{
-    //     name: 'sales',
-    //     data: this.datasValue
-    //   }],
-    //   xaxis: {
-    //     categories: this.keysValue
-    //   }
-    // }
-    // var heatmap = new ApexCharts(this.heatmapTarget, heatmapOptions);
-    // heatmap.render();
-
+    const info = JSON.parse(this.infoValue)
+    console.log(info)
+    const data = Array.from(Object.keys(info), (week) => {
+      return {
+      name: week,
+        data: info[week]
+      };
+    })
+    console.log(data)
     var heatmapOptions = {
-      series: [{
-      name: 'M',
-      data: generateData(6, {
-        min: 0,
-        max: 15
-      })
-    },
-    {
-      name: 'Tu',
-      data: generateData(6, {
-        min: 0,
-        max: 15
-      })
-    },
-    {
-      name: 'We',
-      data: generateData(6, {
-        min: 0,
-        max: 15
-      })
-    },
-    {
-      name: 'Thus',
-      data: generateData(6, {
-        min: 0,
-        max: 15
-      })
-    },
-    {
-      name: 'Fri',
-      data: generateData(6, {
-        min: 0,
-        max: 15
-      })
-    },
-    ],
+      series: data,
       chart: {
       height: 350,
       type: 'heatmap',
@@ -91,26 +32,6 @@ export default class extends Controller {
 
     var chart = new ApexCharts(document.querySelector("#chart"), heatmapOptions);
     chart.render();
-    // var pieOptions = {
-    //   chart: {
-    //     type: 'pie',
-    //     // toolbar: {show:false}
-    //   },
-    //     series: [44, 55, 13, 33],
-    //     labels: ['Apple', 'Mango', 'Orange', 'Watermelon']
-    //   }
-    // var pie = new ApexCharts(this.pieTarget, pieOptions);
-    // pie.render();
 
-    // var donutOptions = {
-    //   chart: {
-    //     type: 'donut',
-    //     // toolbar: {show:false}
-    //   },
-    //   series: [44, 55, 13, 33],
-    //   labels: ['Apple', 'Mango', 'Orange', 'Watermelon']
-    // }
-    // var donut = new ApexCharts(this.donutTarget, donutOptions);
-    // donut.render();
-  }
+}
 }
