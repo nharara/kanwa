1ae9770030d9f5df2735274dcaf7c463bd6f1a62
Andres Henriquez <rymbell@gmail.com>
2023-08-23 11:26:32 +0900

    modified schema to delete tags and add a reference to emotions

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -2,4 +2,5 @@ class Emotion < ApplicationRecord
   belongs_to :parent_emotion, optional: true, class_name: 'Emotion', foreign_key: :emotion_id
   has_many :child_emotions, class_name: 'Emotion', foreign_key: :emotion_id, dependent: :destroy
   has_many :entries, dependent: :destroy
+  has_many :posts
 end
