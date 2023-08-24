34c40a2fe45b2c76abfd32b6d3a9b870690329f7
Andres Henriquez <rymbell@gmail.com>
2023-08-24 17:07:20 +0900

    heatmap with today marker

--- app/javascript/controllers/apexchart_controller.js
+++ app/javascript/controllers/apexchart_controller.js
@@ -4,34 +4,71 @@ import ApexCharts from 'apexcharts';
 // Connects to data-controller="heatmap"
 export default class extends Controller {
   static values={keys: Array, datas: Array, info: String}
+
   static targets=["heatmap", "pie", "donut"]
+
   connect() {
+
+    console.log(new Date().getDay());
+
     const info = JSON.parse(this.infoValue)
-    console.log(info)
     const data = Array.from(Object.keys(info), (week) => {
+      console.log(week);
       return {
-      name: week,
-        data: info[week]
+      name: '',
+      data: info[week]
       };
     })
-    console.log(data)
+
+    const weekday = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
+
+    const d = new Date()
+    let day = weekday[d.getDay() - 1]
+
+    console.log(day);
+
     var heatmapOptions = {
       series: data,
       chart: {
       height: 350,
+      width: '100%',
       type: 'heatmap',
-    },
-    dataLabels: {
-      enabled: false
-    },
-    colors: ["#008FFB"],
-    title: {
-      text: 'HeatMap Chart (Single color)'
-    },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      colors: ["#002080"],
+      title: {
+        text: 'HeatMap Chart (Single color)'
+      },
+      annotations: {
+        points: [
+          {
+            x: day,
+            y: 5.7,
+            marker: {
+              size: 6,
+              fillColor: "#fff",
+              strokeColor: "#2698FF",
+              radius: 2
+            },
+            label: {
+              borderColor: "#FF4560",
+              offsetY: 0,
+              style: {
+                color: "#fff",
+                background: "#FF4560"
+              },
+
+              text: "Today"
+            }
+          }
+        ]
+      }
     };
+
 
     var chart = new ApexCharts(document.querySelector("#chart"), heatmapOptions);
     chart.render();
-
-}
+  }
 }
