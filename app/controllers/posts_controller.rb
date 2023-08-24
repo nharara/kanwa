8496b6bb80e1c52f1daa386cf61efa5ed23d7fcb
Andres Henriquez <rymbell@gmail.com>
2023-08-24 12:06:08 +0900

    fixed posts functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,30 +4,29 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(created_at: :desc, title: :asc)
 
-    @posts = @posts.where("title ILIKE ?", "%#{params[:query]}%") if params[:query].present?
+    @posts = @posts.where("title ILIKE ?", "%#{params[:title]}%") if params[:title].present?
 
     respond_to do |format|
       format.html
-      format.json { render json: @posts }
       format.text { render partial: "posts/posts", locals: { posts: @posts }, formats: [:html] }
     end
 
     @post = Post.new
-
     @parent_emotions = Emotion.where(parent_emotion: nil)
-    respond_to do |format|
-      format.html # Follow regular flow of Rails
-      @child_emotions = Emotion.find(params[:query]).child_emotions if params[:query]
-      format.text { render partial: "entries/new_child", locals: { emotions: @child_emotions }, formats: [:html] }
+    if params[:query].present?
+      respond_to do |format|
+        format.html # Follow regular flow of Rails
+        @child_emotions = Emotion.find(params[:query]).child_emotions if params[:query]
+        format.text { render partial: "entries/new_child", locals: { emotions: @child_emotions }, formats: [:html] }
+      end
     end
   end
 
   def create
     @post = Post.new(post_params)
     @post.user = current_user
-    emotion = Emotion.find(params[:specific])
+    emotion = Emotion.find(params[:entry][:specific_emotion_id])
     @post.emotion = emotion
-
     if @post.save
       redirect_to posts_path
     else
@@ -40,4 +39,5 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :content)
   end
+
 end
