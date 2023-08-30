d818d1a9b57fef5e973bff03e3cb9b079e7fd070
Efren Franco <efrenrfranco@gmail.com>
2023-08-30 17:48:09 +0900

    added colors to emotions

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -6,4 +6,16 @@ class Emotion < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   EMOTIONS = ["angry", "disgusted", "fearful", "bad", "surprised", "happy", "sad"]
+
+    EMOTION_COLOR = {
+      "angry" => "#EAB9B9", #light red
+      "disgusted" => "#F07167", #dark red
+      "fearful" => "#a5cca9", #green
+      "bad" => "#b9b9ea", #gray?
+      "surprised" => "#ffcb77", #yellow
+      "happy" => "#b9eaea", #light blue
+      "sad" => "8AB2BB" #dark blue
+    }
+
+
 end
