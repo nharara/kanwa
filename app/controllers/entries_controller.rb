0143fe7eaccd3de627146257a14dbe59d11a25f2
pj taron <jadepeck27@gmail.com>
2023-08-31 10:49:40 +0900

    add styleing to entry page 3

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,7 +3,7 @@ class EntriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @entries = Entry.where(user: current_user).order("created_at DESC")
+    @entries = Entry.where(user: current_user).order("entries.created_at DESC")
     # @entries = @entries.includes([:emotion])
     if params[:query].present?
       if Emotion::EMOTIONS.include?(params[:query])
