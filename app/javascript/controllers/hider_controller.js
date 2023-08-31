68708709dbd136a6991db9a939bc2fac39858265
Nozomu Hara <slipper-spells-0j@icloud.com>
2023-08-31 21:46:59 +0900

    minor change to entry form

--- app/javascript/controllers/hider_controller.js
+++ app/javascript/controllers/hider_controller.js
@@ -18,36 +18,36 @@ export default class extends Controller {
 
     const limit = this.sectionTargets.length
 
-    if(this.index < limit || this.index > 1 ){
+    if (this.index < limit && this.index >= 1) {
       this.sectionTargets.map(target => {
         target.classList.add('d-none')
       })
 
       this.sectionTargets[this.index].classList.remove('d-none')
 
-      if(child){
+      if (child) {
         child.classList.add('d-none')
       }
-      this.index +=1
+      this.index += 1
     }
   }
 
-  show(){
+  show() {
     console.log('click');
 
     const child = document.getElementById('childEmo')
 
-    if(this.index >= 2){
+    if (this.index >= 2) {
 
-    this.index -= 2
-    this.sectionTargets.map(target => {
-      target.classList.add('d-none')
-    })
-    this.sectionTargets[this.index].classList.remove('d-none')
-    if(child){
-      child.classList.add('d-none')
-    }
-    this.index += 1
+      this.index -= 2
+      this.sectionTargets.map(target => {
+        target.classList.add('d-none')
+      })
+      this.sectionTargets[this.index].classList.remove('d-none')
+      if (child) {
+        child.classList.add('d-none')
+      }
+      this.index += 1
     }
   }
 }
