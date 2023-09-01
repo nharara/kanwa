cfbce38481ac5ae13c54ce83a1518a9c74d960ee
Nozomu Hara <slipper-spells-0j@icloud.com>
2023-09-01 10:58:16 +0900

    removed hider js

--- app/javascript/controllers/hider_controller.js
+++ app/javascript/controllers/hider_controller.js
@@ -1,53 +1,53 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="hider"
-export default class extends Controller {
-  static targets = ['section', 'child']
-  static values = {}
+// // Connects to data-controller="hider"
+// export default class extends Controller {
+//   static targets = ['section', 'child']
+//   static values = {}
 
-  connect() {
-    console.log('toggler connected');
-    this.index = 1
-  }
+//   connect() {
+//     console.log('toggler connected');
+//     this.index = 1
+//   }
 
-  hide() {
+//   hide() {
 
-    console.log('click');
-    const child = document.getElementById('childEmo')
-    console.log(child);
+//     console.log('click');
+//     const child = document.getElementById('childEmo')
+//     console.log(child);
 
-    const limit = this.sectionTargets.length
+//     const limit = this.sectionTargets.length
 
-    if (this.index < limit && this.index >= 1) {
-      this.sectionTargets.map(target => {
-        target.classList.add('d-none')
-      })
+//     if (this.index < limit && this.index >= 1) {
+//       this.sectionTargets.map(target => {
+//         target.classList.add('d-none')
+//       })
 
-      this.sectionTargets[this.index].classList.remove('d-none')
+//       this.sectionTargets[this.index].classList.remove('d-none')
 
-      if (child) {
-        child.classList.add('d-none')
-      }
-      this.index += 1
-    }
-  }
+//       if (child) {
+//         child.classList.add('d-none')
+//       }
+//       this.index += 1
+//     }
+//   }
 
-  show() {
-    console.log('click');
+//   show() {
+//     console.log('click');
 
-    const child = document.getElementById('childEmo')
+//     const child = document.getElementById('childEmo')
 
-    if (this.index >= 2) {
+//     if (this.index >= 2) {
 
-      this.index -= 2
-      this.sectionTargets.map(target => {
-        target.classList.add('d-none')
-      })
-      this.sectionTargets[this.index].classList.remove('d-none')
-      if (child) {
-        child.classList.add('d-none')
-      }
-      this.index += 1
-    }
-  }
-}
+//       this.index -= 2
+//       this.sectionTargets.map(target => {
+//         target.classList.add('d-none')
+//       })
+//       this.sectionTargets[this.index].classList.remove('d-none')
+//       if (child) {
+//         child.classList.add('d-none')
+//       }
+//       this.index += 1
+//     }
+//   }
+// }
